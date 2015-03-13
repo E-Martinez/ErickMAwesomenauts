e4a8ab617ba0b67a3d083df14681c61ebc9e5987
Erick Martinez <jclash626@gmail.com>
2015-03-13 08:38:56 -0700

    Commit

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -16,6 +16,7 @@ game.playerEntity = me.Entity.extend({
 
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+        this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
 
         this.renderable.setCurrentAnimation("idle");
     },
@@ -36,8 +37,26 @@ game.playerEntity = me.Entity.extend({
         } else {
             this.body.vel.x - 0;
         }
+        
+        if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling){
+            this.jumping = true;
+            this.body.vel.y -= this.body.accel.y * me.timer.tick;
+        }
+        
+        
+        if(me.input.isKeyPressed("attack")){
+            if(!this.renderable.isCurrentAnimation("attack")){
+                //Sets the current animation to attack and once that is over
+                //goes back to the idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //Makes it so that the next time we start this sequence we begin
+                //from the first animation, not wherever we left off when we
+                //switched to another animation
+                this.renderable.setAnimationFrame();
+            }
+        }
 
-        if (this.body.vel.x !== 0) {
+        else if (this.body.vel.x !== 0) {
             if (!this.renderable.isCurrentAnimation("walk")) {
                 this.renderable.setCurrentAnimation("walk");
             }
@@ -45,7 +64,17 @@ game.playerEntity = me.Entity.extend({
             this.renderable.setCurrentAnimation("idle");
         }
 
-
+        if(me.input.isKeyPressed("attack")){
+            if(!this.renderable.isCurrentAnimation("attack")){
+                //Sets the current animation to attack and once that is over
+                //goes back to the idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //Makes it so that the next time we start this sequence we begin
+                //from the first animation, not wherever we left off when we
+                //switched to another animation
+                this.renderable.setAnimationFrame();
+            }
+        }
 
         this.body.update(delta);
 
@@ -63,7 +92,7 @@ game.PlayerBaseEntity = me.Entity.extend({
             spritewidth: "100",
             spriteheight: "100",
             getShape: function(){
-                return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                return (new me.Rect(0, 0, 100, 70)).toPolygon();
             }
         }]);
         this.broken = false;
@@ -104,7 +133,7 @@ game.EnemyBaseEntity = me.Entity.extend({
             spritewidth: "100",
             spriteheight: "100",
             getShape: function(){
-                return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                return (new me.Rect(0, 0, 100, 70)).toPolygon();
             }
         }]);
         this.broken = false;
